bfe8559fe356032b29f4cc87c62cd64378bcd821
theBigBengi <imthebigbengi@gmail.com>
2023-05-27 22:10:33 +0300

    cleaning code

--- src/decorators/use.decorator.ts
+++ src/decorators/use.decorator.ts
@@ -1,26 +1,20 @@
 import { MetadataKeys } from "./MetadataKeys";
 import { RequestHandler } from "express";
+import { LoginController } from "../controllers/LoginController";
 
 export function use(middleware: RequestHandler) {
-  return function (
-    originalMethod: Function,
-    context: ClassMethodDecoratorContext
-  ) {
+  return function (value: Function, context: ClassMethodDecoratorContext) {
     // Limit use the of the decorator
     if (context.kind === "method") {
       // Get middlewares metadata
       const middlewares =
-        Reflect.getMetadata(MetadataKeys.middleware, originalMethod) || [];
+        Reflect.getMetadata(MetadataKeys.middleware, value) || [];
 
       // Add the new middleware
       middlewares.push(middleware);
 
       // Define the new middlewares metadata
-      Reflect.defineMetadata(
-        MetadataKeys.middleware,
-        middlewares,
-        originalMethod
-      );
+      Reflect.defineMetadata(MetadataKeys.middleware, middlewares, value);
     }
   };
 }
